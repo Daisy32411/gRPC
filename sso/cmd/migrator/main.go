d87ece1a0895bc382c321cb79f970539a136f718
Kamil <kamilzaripov228@gmail.com>
2026-05-01 19:13:13 +0300

    .

---
+++ sso/cmd/migrator/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"log"
+
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/sqlite3"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+)
+
+func main() {
+	var storagePath, migrationsPath, migrationsTable string
+	var down bool
+
+	flag.StringVar(&storagePath, "storage-path", "", "path to storage")
+	flag.StringVar(&migrationsPath, "migrations-path", "", "path to migrations")
+	flag.StringVar(&migrationsTable, "migrations-table", "migrations", "name of migrations table")
+	flag.BoolVar(&down, "down", false, "rollback last migration")
+	flag.Parse()
+
+	if storagePath == "" {
+		panic("storage-path is required")
+	}
+
+	if migrationsPath == "" {
+		panic("migrations-path is required")
+	}
+
+	m, err := migrate.New(
+		"file://" + migrationsPath,
+		fmt.Sprintf("sqlite3://%s?x-migrations-table=%s", storagePath, migrationsTable),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	if down {
+		if err := m.Steps(-1); err != nil {
+			if errors.Is(err, migrate.ErrNoChange) {
+				fmt.Println("no migrations to rollback")
+				return
+			}
+			log.Fatal(err)
+		}
+		fmt.Println("migration rolled back successfully")
+		return
+	}
+	if err := m.Up(); err != nil {
+		if errors.Is(err, migrate.ErrNoChange) {
+			fmt.Println("no migrations to apply")
+
+			return
+		}
+
+		panic(err)
+	}
+
+	fmt.Println("migrations applied successfully")
+}
